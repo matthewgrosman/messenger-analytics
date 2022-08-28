f99cc23ea3c135d285e043841892d44f3f233cc3
Matthew Grosman <grosmanmatt@gmail.com>
2022-08-28 00:35:44 -0700

    Added in tests for parseConversationFolderFiles method

--- src/test/java/ConversationParserTest.java
+++ src/test/java/ConversationParserTest.java
@@ -8,6 +8,7 @@ import shared.Constants;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -17,7 +18,7 @@ public class ConversationParserTest {
         String filePath = "src/test/test-input-files";
         File[] files = ConversationParser.getConversationFolders(filePath);
 
-        Assertions.assertEquals(files.length, 4);
+        Assertions.assertEquals(files.length, 5);
     }
 
     @Test
@@ -124,5 +125,54 @@ public class ConversationParserTest {
         ArrayList<Document> documents = ConversationParser.parseMessageJsonFile(testInputJsonFile);
 
         Assertions.assertEquals(documents.size(), 0);
+    }
+
+    @Test
+    public void testParseConversationFolderFilesWtihValidDirectory() throws IOException {
+        File validConversationPath = new File("src/test/test-input-files/valid-conversation");
+        ArrayList<Document> messages = ConversationParser.parseConversationFolderFiles(validConversationPath);
+
+        // Check first message document is correct
+        Document message1 = messages.get(0);
+
+        String expectedConversationName1 = "Person1";
+        String expectedSenderName1 = "Matthew Grosman";
+        String expectedDate1 = new Date(1538281056549L).toString();
+        String expectedContent1 = "New content";
+
+        Assertions.assertEquals(expectedConversationName1, message1.get(Constants.MONGO_CONVERSATION_FIELD_NAME));
+        Assertions.assertFalse((boolean)  message1.get(Constants.MONGO_GROUP_CHAT_FIELD_NAME));
+        Assertions.assertEquals(expectedSenderName1, message1.get(Constants.MONGO_SENDER_FIELD_NAME));
+        Assertions.assertEquals(expectedDate1, message1.get(Constants.MONGO_DATE_FIELD_NAME).toString());
+        Assertions.assertEquals(expectedContent1, message1.get(Constants.MONGO_CONTENT_FIELD_NAME));
+
+        // Check second message document is correct
+        Document message2 = messages.get(1);
+
+        String expectedConversationName2 = "Person1";
+        String expectedSenderName2 = "Matthew Grosman";
+        String expectedDate2 = new Date(1538281056531L).toString();
+        String expectedContent2 = "Some content string";
+
+        Assertions.assertEquals(expectedConversationName2, message2.get(Constants.MONGO_CONVERSATION_FIELD_NAME));
+        Assertions.assertFalse((boolean)  message2.get(Constants.MONGO_GROUP_CHAT_FIELD_NAME));
+        Assertions.assertEquals(expectedSenderName2, message2.get(Constants.MONGO_SENDER_FIELD_NAME));
+        Assertions.assertEquals(expectedDate2, message2.get(Constants.MONGO_DATE_FIELD_NAME).toString());
+        Assertions.assertEquals(expectedContent2, message2.get(Constants.MONGO_CONTENT_FIELD_NAME));
+
+        // Check second message document is correct
+        Document message3 = messages.get(2);
+
+        String expectedConversationName3 = "Person1";
+        String expectedSenderName3 = "Person1";
+        String expectedDate3 = new Date(1538252862432L).toString();
+        String expectedContent3 = "This is a message";
+
+        Assertions.assertEquals(expectedConversationName3, message3.get(Constants.MONGO_CONVERSATION_FIELD_NAME));
+        Assertions.assertFalse((boolean)  message3.get(Constants.MONGO_GROUP_CHAT_FIELD_NAME));
+        Assertions.assertEquals(expectedSenderName3, message3.get(Constants.MONGO_SENDER_FIELD_NAME));
+        Assertions.assertEquals(expectedDate3, message3.get(Constants.MONGO_DATE_FIELD_NAME).toString());
+        Assertions.assertEquals(expectedContent3, message3.get(Constants.MONGO_CONTENT_FIELD_NAME));
+
     }
 }
